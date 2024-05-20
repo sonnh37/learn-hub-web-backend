11d49786e4d92d3f22b1bfbec2fcefc73bb7bfba
Thanh Thu <thanhthu26082003@gmail.com>
2024-05-20 21:30:44 +0700

    build-repository

---
+++ ConsoleApp1/Repositories/Repositories/SubjectRepository.cs
@@ -0,0 +1,19 @@
+﻿using SmartThrive.DataAccess.Repositories.Base;
+using SmartThrive.DataAccess.Repositories.Repositories.Interface;
+using ST.Entities.Data;
+using ST.Entities.Data.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartThrive.DataAccess.Repositories.Repositories
+{
+    public class SubjectRepository : BaseRepository<Subject>, ISubjectRepository
+    {
+        public SubjectRepository(STDbContext context) : base(context)
+        {
+        }
+    }
+}
