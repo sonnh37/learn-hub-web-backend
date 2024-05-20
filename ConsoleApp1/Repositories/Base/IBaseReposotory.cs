7a6504667ba59d5deb7315f5a6c51716357bd8e9
Son Nguyen <nguyenson20zzzz@gmail.com>
2024-05-20 22:37:03 +0700

    add repositories

---
+++ ConsoleApp1/Repositories/Base/IBaseReposotory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartThrive.DataAccess.Repositories.Base
+{
+    public interface IBaseRepository<TEntity> where TEntity : class
+    {
+        Task<IList<TEntity>> GetAll();
+        Task<TEntity> GetById(Guid id);
+        Task<bool> Add(TEntity entity);
+        Task<bool> Delete(Guid id);
+        Task<bool> Update(TEntity entity);
+
+    }
+}
